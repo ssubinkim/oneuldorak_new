738eca6207bc24bdb020ce76a3bb8bc41dfbabe4
josebin0530-create <osebin0530@gmail.com>
2026-05-12 14:27:09 +0900

    스토어페이지 사진수정

--- src/components/store/BrandPickSection.tsx
+++ src/components/store/BrandPickSection.tsx
@@ -1,11 +1,16 @@
 import './BrandPickSection.css'
+import logo1 from './images/storelogo_1.png'
+import logo2 from './images/storelogo_2.png'
+import logo3 from './images/storelogo_3.png'
+import logo4 from './images/storelogo_4.png'
+import logo5 from './images/storelogo_5.png'
 
 const BRANDS = [
-  { id: '1', name: '밥픽', icon: '🍱', bg: '#fff5cc' },
-  { id: '2', name: '키친무드', icon: '🫕', bg: '#ffe8e8' },
-  { id: '3', name: '요미키친', icon: '🍲', bg: '#e8f4ff' },
-  { id: '4', name: '한끼랩', icon: '🥗', bg: '#e8ffe8' },
-  { id: '5', name: '혹포켓', icon: '🍜', bg: '#f5e8ff' },
+  { id: '1', name: '밥픽', logo: logo1 },
+  { id: '2', name: '키친무드', logo: logo2 },
+  { id: '3', name: '요미키친', logo: logo3 },
+  { id: '4', name: '한끼랩', logo: logo4 },
+  { id: '5', name: '혹포켓', logo: logo5 },
 ]
 
 function BrandPickSection() {
@@ -21,8 +26,8 @@ function BrandPickSection() {
       <div className="brand-pick__track">
         {BRANDS.map(brand => (
           <div key={brand.id} className="brand-pick__item">
-            <div className="brand-pick__circle" style={{ background: brand.bg }}>
-              <span className="brand-pick__icon">{brand.icon}</span>
+            <div className="brand-pick__circle">
+              <img className="brand-pick__logo" src={brand.logo} alt={brand.name} />
             </div>
             <p className="brand-pick__name">{brand.name}</p>
           </div>
--- src/components/store/StoreMain.tsx
+++ src/components/store/StoreMain.tsx
@@ -13,30 +13,40 @@ import best2 from './images/best_item2.png'
 import best3 from './images/best_item3.png'
 import best4 from './images/best_item4.png'
 import best5 from './images/best_item5.png'
+import sub1 from './images/sub_1.svg'
+import sub2 from './images/sub_2.svg'
+import sub3 from './images/sub_3.svg'
+import sub4 from './images/sub_4.svg'
+import sub5 from './images/sub_5.svg'
+import kitchen1 from './images/kitchen_1.svg'
+import kitchen2 from './images/kitchen_2.svg'
+import kitchen3 from './images/kitchen_3.svg'
+import kitchen4 from './images/kitchen_4.svg'
+import kitchen5 from './images/kitchen_5.svg'
 
 const BEST_PRODUCTS: Product[] = [
-  { id: '1', brand: '밥픽', name: '맛드러지는 밀프랩', price: 12000, image: best1, rating: 4.9, reviewCount: 267 },
-  { id: '2', brand: '밥픽', name: '맛드러지는 밀프랩', price: 12000, image: best2, rating: 4.9, reviewCount: 267 },
-  { id: '3', brand: '밥픽', name: '맛드러지는 밀프랩', price: 12000, image: best3, rating: 4.9, reviewCount: 267 },
-  { id: '4', brand: '밥픽', name: '맛드러지는 밀프랩', price: 12000, image: best4, rating: 4.9, reviewCount: 267 },
-  { id: '5', brand: '밥픽', name: '맛드러지는 밀프랩', price: 12000, image: best5, rating: 4.9, reviewCount: 267 },
+  { id: '1', brand: '모노메이트', name: '간편 도시락 세트', price: 32000, image: best1, rating: 4.9, reviewCount: 267 },
+  { id: '2', brand: '밥픽', name: '계란말이 도시락', price: 4900, image: best2, rating: 4.9, reviewCount: 267 },
+  { id: '3', brand: '밥픽', name: '참치마요 덮밥', price: 5900, image: best3, rating: 4.9, reviewCount: 267 },
+  { id: '4', brand: '모노메이트', name: '전자레인지 런치박스', price: 9900, image: best4, rating: 4.9, reviewCount: 267 },
+  { id: '5', brand: '프레시잇', name: '오늘의야채 샐러드', price: 12000, image: best5, rating: 4.9, reviewCount: 267 },
 ]
 
 const SUBSCRIPTION_PRODUCTS: Product[] = [
-  { id: '1', brand: '밥픽', name: '맛드러지는 밀프랩', price: 12000, image: best1, rating: 4.9, reviewCount: 267 },
-  { id: '2', brand: '밥픽', name: '맛드러지는 밀프랩', price: 12000, image: best2, rating: 4.9, reviewCount: 267 },
-  { id: '3', brand: '밥픽', name: '맛드러지는 밀프랩', price: 12000, image: best3, rating: 4.9, reviewCount: 267 },
-  { id: '4', brand: '밥픽', name: '맛드러지는 밀프랩', price: 12000, image: best4, rating: 4.9, reviewCount: 267 },
-  { id: '5', brand: '밥픽', name: '맛드러지는 밀프랩', price: 12000, image: best5, rating: 4.9, reviewCount: 267 },
-  { id: '6', brand: '밥픽', name: '맛드러지는 밀프랩', price: 12000, image: best1, rating: 4.9, reviewCount: 267 },
+  { id: '1', brand: '모노메이트', name: '2칸 컬러풀 도시락', price: 12000, image: sub1, rating: 4.9, reviewCount: 267 },
+  { id: '2', brand: '파머스픽', name: '아보카도 계란 샐러드', price: 8600, image: sub2, rating: 4.9, reviewCount: 267 },
+  { id: '3', brand: '파머스픽', name: '훈제연어 샐러드', price: 9600, image: sub3, rating: 4.9, reviewCount: 267 },
+  { id: '4', brand: '프레시잇', name: '제육볶음 밀키트', price: 10000, image: sub4, rating: 4.9, reviewCount: 267 },
+  { id: '5', brand: '프레시잇', name: '4종 밀키트 골라담기', price: 30000, image: sub5, rating: 4.9, reviewCount: 267 },
+  { id: '6', brand: '모노메이트', name: '간편 도시락 세트', price: 32000, image: best1, rating: 4.9, reviewCount: 267 },
 ]
 
 const KITCHEN_PRODUCTS: Product[] = [
-  { id: '1', brand: '밥픽', name: '맛드러지는 밀프랩', price: 12000, image: best1, rating: 4.9, reviewCount: 267 },
-  { id: '2', brand: '밥픽', name: '맛드러지는 밀프랩', price: 12000, image: best2, rating: 4.8, reviewCount: 182 },
-  { id: '3', brand: '밥픽', name: '맛드러지는 밀프랩', price: 12000, image: best3, rating: 4.7, reviewCount: 95 },
-  { id: '4', brand: '밥픽', name: '맛드러지는 밀프랩', price: 12000, image: best4, rating: 4.9, reviewCount: 267 },
-  { id: '5', brand: '밥픽', name: '맛드러지는 밀프랩', price: 12000, image: best5, rating: 4.6, reviewCount: 43 },
+  { id: '1', brand: '모노메이트', name: '1-2인용 에어프라이기', price: 44000, image: kitchen1, rating: 4.9, reviewCount: 267 },
+  { id: '2', brand: '모노메이트', name: '저당 칼로볼', price: 29000, image: kitchen2, rating: 4.8, reviewCount: 182 },
+  { id: '3', brand: '모노메이트', name: '500ml 보온 텀블러', price: 15000, image: kitchen3, rating: 4.7, reviewCount: 95 },
+  { id: '4', brand: '모노메이트', name: '도시락 보냉가방', price: 11000, image: kitchen4, rating: 4.9, reviewCount: 267 },
+  { id: '5', brand: '모노메이트', name: '밀프랩 전용 법랑용기', price: 6000, image: kitchen5, rating: 4.6, reviewCount: 43 },
 ]
 
 type Props = {
