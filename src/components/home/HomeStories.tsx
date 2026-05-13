498d9b3e7c0eaed016620eb78bbd8c791260977e
suse08240412-dev <suse08240412@gmail.com>
2026-05-14 00:25:55 +0900

    폰트 수정

--- src/components/home/HomeStories.tsx
+++ src/components/home/HomeStories.tsx
@@ -15,7 +15,7 @@ const topStories = stories.slice(0, 3)
 
 function HomeStories() {
   return (
-    <section className="stories-section font-pretendard" aria-labelledby="storiesTitle">
+    <section className="stories-section font-pretendard-variable" aria-labelledby="storiesTitle">
       <div className="stories-section__intro">
         <div className="stories-section__copy">
           <h2 id="storiesTitle">
