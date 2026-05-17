1796237eca53c3a718690312d192b3c6fa09bb59
suse08240412-dev <suse08240412@gmail.com>
2026-05-18 03:04:24 +0900

    api 키 올림, 연결

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import svgr from 'vite-plugin-svgr'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:4242',
+        changeOrigin: true,
+      },
+    },
+  },
 })
