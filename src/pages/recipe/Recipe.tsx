a9aa4f3f2056f0b3e3aa293f7ae003f867d49287
ssubinkim <dkssud2983@gmail.com>
2026-05-25 01:06:51 +0900

    레시피 게시글 비활성화

--- src/pages/recipe/Recipe.tsx
+++ src/pages/recipe/Recipe.tsx
@@ -68,7 +68,7 @@ function Recipe() {
                 <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
               </svg>
             </button>
-            <button type="button" aria-label="레시피 작성" onClick={() => { window.location.hash = '#/recipe-write' }}>
+            <button type="button" aria-label="레시피 작성" style={{ opacity: 0.35 }} onClick={() => { window.location.hash = '#/recipe-write' }}>
               <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true">
                 <path d="M12 5v14M5 12h14" />
               </svg>
